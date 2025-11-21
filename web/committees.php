8da38cb7b4f1f0b2a889eff11d0d57789f5ccd64
AndroRazk <razkandro2002@gmail.com>
2025-11-21 13:08:38 -0500

    Andro

--- web/committees.php
+++ web/committees.php
@@ -5,21 +5,22 @@ $cn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 echo "<h1>Committees</h1>";
 
 $sql = "SELECT * FROM committees";
-$result = mysqli_query($cn, $sql);
+$res = mysqli_query($cn, $sql);
 
 echo "<table border='1' cellpadding='6'>";
-echo "<tr><th>Committee Name</th><th>Description</th><th>Action</th></tr>";
+echo "<tr><th>Name</th><th>Description</th><th>Action</th></tr>";
 
-while ($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($res)) {
     $id = (int)$row['committee_id'];
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($row['committee_name']) . "</td>";
-    echo "<td>" . htmlspecialchars($row['description']) . "</td>";
+    echo "<td>".htmlspecialchars($row['committee_name'])."</td>";
+    echo "<td>".htmlspecialchars($row['description'])."</td>";
     echo "<td><a href='committee_members.php?committee_id=$id'>View Members</a></td>";
     echo "</tr>";
 }
-echo "</table>";
 
+echo "</table>";
 echo "<p><a href='items.php'>Back to Items</a></p>";
+
 mysqli_close($cn);
 ?>
